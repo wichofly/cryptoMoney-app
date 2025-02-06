7583feeb72b105c1a38345152e734a4c3a7783e0
Mauricio Erazo <antonierazo13@hotmail.com>
2025-02-06 17:56:51 +0100

    Add Currency interface and initial currency data

---
+++ src/data/data.ts
@@ -0,0 +1,8 @@
+import { Currency } from '../interfaces/interface';
+
+export const currencies: Currency[] = [
+  { code: 'USD', name: 'United States Dollar' },
+  { code: 'MXN', name: 'Mexican Peso' },
+  { code: 'EUR', name: 'Euro' },
+  { code: 'GBP', name: 'British Pound' },
+];
